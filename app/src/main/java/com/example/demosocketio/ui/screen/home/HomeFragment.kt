d33cc700cc7f0b113e5e96dadaf530a74a428144
Thanh Van <thanhnv130120@gmail.com>
2022-03-02 12:51:37 +0700

    - update

--- app/src/main/java/com/example/demosocketio/ui/screen/home/HomeFragment.kt
+++ app/src/main/java/com/example/demosocketio/ui/screen/home/HomeFragment.kt
@@ -1,5 +1,9 @@
 package com.example.demosocketio.ui.screen.home
 
+import android.content.Intent
+import android.content.Intent.getIntent
+import android.net.Uri
+import android.util.Log
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProvider
@@ -9,6 +13,12 @@ import com.example.demosocketio.data.base.BaseFragment
 import com.example.demosocketio.data.model.LoadDataStatus
 import com.example.demosocketio.data.response.DataResponse
 import com.example.demosocketio.databinding.FragmentHomeBinding
+import com.example.demosocketio.utils.Constants
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData
+
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
@@ -17,6 +27,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun getLayoutID() = R.layout.fragment_home
 
     override fun initView() {
+
+        getDynamicLink()
 
         binding!!.edtName.doAfterTextChanged {
             viewModel.updateUserName(it.toString())
@@ -27,7 +39,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         }
 
         binding!!.btnDeeplink.setOnClickListener {
-
+            val browserIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(Constants.URL_DEEPLINK)
+            )
+            startActivity(browserIntent)
         }
 
         binding!!.lifecycleOwner = this
@@ -50,11 +66,34 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
                         findNavController().navigate(action)
                     }
                     else -> {
-                        Toast.makeText(requireContext(), getString(R.string.empty_input), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.empty_input),
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
         }
+    }
 
+    private fun getDynamicLink() {
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(requireActivity().intent)
+            .addOnSuccessListener(
+                requireActivity()
+            ) { pendingDynamicLinkData ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+                if (deepLink != null)
+                    Log.d("TAG", "getDynamicLink: $deepLink")
+
+            }
+            .addOnFailureListener(
+                requireActivity()
+            ) { e -> Log.d("TAG", "getDynamicLink:onFailure", e) }
     }
 }
--- app/src/main/java/com/example/demosocketio/utils/Constants.kt
+++ app/src/main/java/com/example/demosocketio/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.demosocketio.utils
 object Constants {
 
     const val CHAT_SERVER_URL = "http://192.168.1.7:3000"
+    const val URL_DEEPLINK = "https://demosocketio.page.link/testdeeplink"
 }
